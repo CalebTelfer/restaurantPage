2b0bf1b5b54a71e19ae5d2dcf018280350df1847
caleb <calebtelfe@gmail.com>
2025-05-18 17:55:52 -0400

    Home page finished and styled.

--- src/page-load.js
+++ src/page-load.js
@@ -1,5 +1,6 @@
 export function loadPage() {
     const h1 = document.createElement("h1");
+    h1.style.marginTop = "2rem";
     h1.textContent = "Caleb's Restaurant";
 
 
@@ -10,7 +11,8 @@ export function loadPage() {
     contentContainer.style.alignItems = "center";
     contentContainer.style.width = "30vw";
     contentContainer.style.height = "50vh";
-    contentContainer.style.backgroundColor = "rgba(143, 201, 255, 0.84)";
+    contentContainer.style.backgroundColor = "rgba(143, 201, 255, 0.85)";
+    contentContainer.style.borderRadius = "10px";
     
 
 
@@ -20,17 +22,20 @@ export function loadPage() {
     h2.textContent = "We are on the HOME page!";
     h2.style.width = "80%";
     h2.style.textAlign = "center";
+    h2.style.marginTop = "2rem";
     contentContainer.appendChild(h2);
 
 
     const hr = document.createElement("hr");
     hr.style.width = "80%";
+    hr.style.marginTop = "1rem";
     contentContainer.appendChild(hr);
 
 
     const p = document.createElement("p");
     p.textContent = "Page content! This is the home page! Home stuff goes here. We are a restaurant who sells food....";
     p.style.width = "80%";
+    p.style.marginTop = "1.5rem";
     contentContainer.appendChild(p);
 
 
@@ -38,4 +43,12 @@ export function loadPage() {
     const content = document.getElementById("content");
     content.appendChild(h1);
     content.appendChild(contentContainer);
+
+
+    const homeButton = document.getElementById("home");
+    homeButton.style.backgroundColor = "rgb(150, 147, 240)";
+    homeButton.style.height = "2.5rem";
+    homeButton.style.width = "8.5rem";
+    homeButton.style.border = "2px white solid";
+
 }
